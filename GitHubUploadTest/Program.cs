b7a3b0351c6ec2c550b1ff33c791d1c51955e7ca
LittleJC3 <admiraldolphin31@gmail.com>
2021-09-16 15:02:01 -0400

    Add project files.

---
+++ GitHubUploadTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubUploadTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
